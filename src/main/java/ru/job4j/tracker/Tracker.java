97bb45b71c676c5e57231001ba7a3a6d7dc260a2
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-03-29 14:20:24 +0400

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -38,15 +38,29 @@ public class Tracker {
         return newArr;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    public int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+            if (items[index].getId() == id) {
+                rsl = index;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(index + 1);
+            items[index] = item;
+
+            return true;
+        }
+        return false;
     }
 }

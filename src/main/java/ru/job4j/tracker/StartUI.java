3a7760751aadd5ca73945c7d95bea36f038ff493
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-03-30 12:07:16 +0400

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateAndTime = formatter.format(item.getCreated());
         System.out.println("Current Date & Time: " + currentDateAndTime);
+        Item task = new Item(9, "the last lesson");
+        System.out.println(task);
     }
 }

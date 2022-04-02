5a106edadf6ace2d3f0e2626a03a2decb68c7371
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-04-02 23:35:41 +0400

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,30 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    double cost = 1.7;
+
+    @Override
+    public void run() {
+        System.out.println("Bus is running");
+    }
+
+    @Override
+    public void passenger(int passenger) {
+        if (passenger < 20) {
+            System.out.println("This is a small bus");
+            System.out.println("Maximum capacity of the bus is up to 20 passengers");
+        } else if (passenger > 20 && passenger < 40) {
+            System.out.println("This is a big bus");
+            System.out.println("Maximum capacity of the bus is up to 40 passengers");
+        } else {
+            System.out.println("One bus us cannot take " + passenger + " passengers. Please take an additional vehicle.");
+        }
+
+    }
+
+    @Override
+    public double petrol(int petrolLitre) {
+        return cost * petrolLitre;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void run();
+
+    void passenger(int passenger);
+
+    double petrol(int petrol);
+}

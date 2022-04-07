32ae4f042e0a4809f0ebc0b33deb86760da21973
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-04-07 10:52:21 +0400

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -207,4 +207,52 @@ public class StartUITest {
                 )
         );
     }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenOneByValidateInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenManyByValidateInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected1 = input.askInt("Enter menu:");
+        assertThat(selected1, is(1));
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected2, is(2));
+        int selected3 = input.askInt("Enter menu:");
+        assertThat(selected3, is(3));
+    }
+
+    @Test
+    public void whenMinus1ByValidateInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
 }

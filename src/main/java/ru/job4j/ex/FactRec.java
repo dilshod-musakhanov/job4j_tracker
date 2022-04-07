35fe908032df87316e933c4a61ccdd890f1c9a09
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-04-07 09:29:54 +0400

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #285105]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,8 +2,8 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        if (n == 1) {
-            return n;
+        if (n == 1 || n == 0) {
+            return 1;
         }
         return calc(n - 1) * n;
     }

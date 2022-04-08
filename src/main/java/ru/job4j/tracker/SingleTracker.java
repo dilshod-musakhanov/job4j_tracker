e8a02cac5585b67f17e6c2fff8f21f72b8eab9d7
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-04-08 11:04:00 +0400

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,53 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public final class SingleTracker {
+    private static SingleTracker instance = null;
+
+    private Tracker tracker = new Tracker();
+
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+      return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public int indexOf(int id) {
+        return tracker.indexOf(id);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+}

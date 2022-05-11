48196fa8439ef438fa798e89fc22b2b903ba07dc
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-05-11 12:54:12 +0400

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -56,7 +56,6 @@ public class Analyze {
                 .flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors.groupingBy(
                                 Subject::getName,
-                                LinkedHashMap::new,
                                 Collectors.summingDouble(Subject::getScore)
                         ))
                 .entrySet().stream().map(t -> new Tuple(t.getKey(), t.getValue()))

f6e51d1bf7af60a6f8cc36e529a7dd93a73cb5b9
Dilshod Musakhanov <musakhanov@yahoo.com>
2023-04-23 20:32:52 +0400

    ToMany [#301848 #411673]

---
+++ src/main/java/ru/job4j/toone/UserMessenger.java
@@ -0,0 +1,26 @@
+package ru.job4j.toone;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "j_user_notification")
+public class UserMessenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
+    private int id;
+    private String messenger;
+    private String identify;
+
+}

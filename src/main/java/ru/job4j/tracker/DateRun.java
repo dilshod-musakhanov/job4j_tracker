df5742a58a72ea7676a72b88d55b308c7c5f52cc
Dilshod Musakhanov <musakhanov@yahoo.com>
2023-05-11 14:07:39 +0400

    TimeZone

---
+++ src/main/java/ru/job4j/tracker/DateRun.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class DateRun {
+    public static void main(String[] args) {
+        var registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (var sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            var session = sf
+                    .withOptions()
+                    .jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+                    .openSession();
+            session.beginTransaction();
+            var stored = session.createQuery(
+                    "from Item", Item.class
+            ).list();
+            for (Item it : stored) {
+                var time = it.getCreated().atZone(
+                        ZoneId.of("UTC+4")
+                ).format(DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd"));
+                System.out.println(time);
+            }
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
